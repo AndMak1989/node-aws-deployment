d707e420c92de78fae08bf51d7e974c385076c4e
Andrii.Makovieiev@medhost.com <amakovieiev@fortifiedhealthsecurity.com>
2021-12-30 12:44:50 +0200

    Dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,13 +20,13 @@ import * as path from 'path';
         ConfigModule.forRoot({
            envFilePath: `.${process.env.NODE_ENV}.env`
         }),
-        // ServeStaticModule.forRoot({
-        //     rootPath: path.resolve( __dirname, '..','static'),
-        // }),
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve( __dirname, 'static'),
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
             host: process.env.POSTGRES_HOST,
-            port: Number(process.env.POSTGRESS_PORT),
+            port: Number(process.env.POSTGRES_PORT),
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
